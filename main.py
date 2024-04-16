9d4d2b2da745d3ddcc5ada31b1cc2efd5f88acbd
unsubstantiated-Script <jusfulton@gmail.com>
2024-04-15 19:30:01 -0700

    Loop itertor to add pages is done

--- main.py
+++ main.py
@@ -14,4 +14,8 @@ for index, row in df.iterrows():
 
     pdf.line(10, 21, 200, 21)
 
+    # Subtracting one as it's added above
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+
 pdf.output('output.pdf', 'F')
